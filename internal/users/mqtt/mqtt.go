772cc43d83e5dc52fbb100b0a2f3ae30d79c7ac6
Francesco <101055738+frankiesgar67@users.noreply.github.com>
2025-12-28 23:20:06 +0100

    Update mqtt.go

--- internal/users/mqtt/mqtt.go
+++ internal/users/mqtt/mqtt.go
@@ -2,42 +2,75 @@ package mqtt
 
 import (
 	"fmt"
-	mqtt "github.com/eclipse/paho.mqtt.golang"
+	paho "github.com/eclipse/paho.mqtt.golang" // Alias paho per evitare conflitti
 	"mqtg-bot/internal/models"
 	"net/url"
-	"time"
 	"os"
+	"strconv"
+	"time"
 )
 
 type Client struct {
-	mqtt.Client
+	paho.Client
 	subscriptionCh chan SubscriptionMessage
 }
 
 type SubscriptionMessage struct {
-	Message      mqtt.Message
+	Message      paho.Message
 	Subscription *models.Subscription
 }
 
-func Connect(dbUser *models.DbUser, subscriptionCh chan SubscriptionMessage) (*Client, error) {
+// Connect inizializza la connessione configurando KeepAlive, CleanSession e la callback di riconnessione
+func Connect(dbUser *models.DbUser, subscriptionCh chan SubscriptionMessage, onConnectCallback func(paho.Client)) (*Client, error) {
 	uri, err := url.Parse(dbUser.MqttUrl)
 	if err != nil {
 		return nil, fmt.Errorf("could not parse MqttUrl: %v", err)
 	}
 
-	clientOptions := mqtt.NewClientOptions()
+	clientOptions := paho.NewClientOptions()
+	
+	// --- CONFIGURAZIONE RESILIENZA E RICONNESSIONE ---
+	clientOptions.SetAutoReconnect(true)
+	clientOptions.SetMaxReconnectInterval(1 * time.Minute)
+	clientOptions.SetOnConnectHandler(onConnectCallback) 
+
+	// Gestione MQTT_CLEAN_SESSION (Default: false per mantenere la sessione sul broker)
+	// SE VUOI CANCELLARE LE CODE ALL'AVVIO:
+	// Imposta la variabile d'ambiente (file .ENV) MQTT_CLEAN_SESSION=true
+	// Questo farà sì che il broker dimentichi i messaggi vecchi ogni volta che il bot parte.
+	cleanSession := false
+	if os.Getenv("MQTT_CLEAN_SESSION") == "true" {
+		cleanSession = true
+	}
+	clientOptions.SetCleanSession(cleanSession)
+
+	// Gestione MQTT_KEEPALIVE (Default: 60 secondi)
+	keepAlive := int64(60)
+	if kaStr := os.Getenv("MQTT_KEEPALIVE"); kaStr != "" {
+		if val, err := strconv.ParseInt(kaStr, 10, 64); err == nil {
+			keepAlive = val
+		}
+	}
+	clientOptions.SetKeepAlive(time.Duration(keepAlive) * time.Second)
+	// ------------------------------------------------
+
 	clientOptions.AddBroker(fmt.Sprintf("%s://%s", uri.Scheme, uri.Host))
 	clientOptions.SetUsername(uri.User.Username())
 	password, _ := uri.User.Password()
 	clientOptions.SetPassword(password)
+	
 	clientId := os.Getenv("MQTT_CLIENT_ID")
 	if clientId != "" {
 		clientOptions.SetClientID(clientId)
+	} else {
+		clientOptions.SetClientID(fmt.Sprintf("mqtg-%d", time.Now().Unix()))
 	}
-	client := mqtt.NewClient(clientOptions)
+
+	client := paho.NewClient(clientOptions)
 
 	token := client.Connect()
-	for !token.WaitTimeout(3 * time.Second) {
+	if !token.WaitTimeout(10 * time.Second) {
+		return nil, fmt.Errorf("MQTT connection timeout")
 	}
 
 	if err := token.Error(); err != nil {
@@ -51,22 +84,18 @@ func Connect(dbUser *models.DbUser, subscriptionCh chan SubscriptionMessage) (*C
 }
 
 func (c *Client) Subscribe(subscription *models.Subscription) {
-	c.Client.Subscribe(subscription.Topic, subscription.Qos, func(client mqtt.Client, msg mqtt.Message) {
+	c.Client.Subscribe(subscription.Topic, subscription.Qos, func(client paho.Client, msg paho.Message) {
 		c.subscriptionCh <- SubscriptionMessage{
 			Message:      msg,
 			Subscription: subscription,
 		}
-		metrics.numOfIncMessagesFromMQTT.Inc()
 	})
-	metrics.numOfMqttSubscriptions.Inc()
 }
 
 func (c *Client) Unsubscribe(subscription *models.Subscription) {
 	c.Client.Unsubscribe(subscription.Topic)
-	metrics.numOfMqttSubscriptions.Dec()
 }
 
 func (c *Client) Publish(topic string, qos byte, retained bool, payload interface{}) {
 	c.Client.Publish(topic, qos, retained, payload)
-	metrics.numOfOutMessagesToMQTT.Inc()
 }
